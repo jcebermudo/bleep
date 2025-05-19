0966a3e8c6c046277a85173a54e30faf6bb8552f
James <jamestech718@gmail.com>
2025-05-19 19:39:17 +0800

    login test

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,39 +1,36 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { createClient } from "@/utils/supabase/client";
+// app/auth/page.tsx
+"use server";
 
-export default function Auth() {
-  const router = useRouter();
-  const supabase = createClient();
+import { createClient } from "@/utils/supabase/server";
 
-  useEffect(() => {
-    const checkUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      if (user) {
-        router.push("/");
-      }
-    };
-    checkUser();
-  }, [router]);
+export default async function Auth() {
+  const supabase = await createClient();
+
+  async function handleSignIn() {
+    "use server";
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/auth/callback`,
+      },
+    });
+
+    if (error) {
+      throw new Error(error.message);
+    }
+  }
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <button
-        className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-        onClick={async () =>
-          await supabase.auth.signInWithOAuth({
-            provider: "google",
-            options: {
-              redirectTo: `${window.location.origin}/auth/callback`,
-            },
-          })
-        }
-      >
-        Sign in with Google
-      </button>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <form action={handleSignIn}>
+        <button
+          type="submit"
+          className="cursor-pointer rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+        >
+          Sign in with Google
+        </button>
+      </form>
     </div>
   );
 }
