723d2c9c944071f1435734f93bac7e8b43fe1748
James <jamestech718@gmail.com>
2025-05-21 20:14:44 +0800

    dropped all of em

--- supabase/migrations/0001_overconfident_namora.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "chat_messages" DROP CONSTRAINT "chat_messages_chat_id_chat_id_fk";
---> statement-breakpoint
-ALTER TABLE "chat" ADD COLUMN "user_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "chat" ADD COLUMN "link_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "chat_messages" ADD CONSTRAINT "chat_messages_chat_id_chat_id_fk" FOREIGN KEY ("chat_id") REFERENCES "public"."chat"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "chat" DROP COLUMN "chat_id";
