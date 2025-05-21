1b59e18144dbba4c08627935729f085b50cbb840
James <jamestech718@gmail.com>
2025-05-21 20:10:05 +0800

    table changes, for now project and chatmessages tables

--- db/schema.ts
+++ db/schema.ts
@@ -1,27 +1,27 @@
 import {
-  integer,
-  uuid,
   pgTable,
   serial,
   text,
   timestamp,
 } from "drizzle-orm/pg-core";
 
-export const chat = pgTable("chat", {
-  id: serial("id").primaryKey(),
-  user_id: text("user_id").notNull(),
-  link_id: text("link_id").notNull(),
-  title: text("title").notNull(),
-  content: text("content").notNull(),
-  created_at: timestamp("created_at").notNull().defaultNow(),
-  updated_at: timestamp("updated_at").notNull().defaultNow(),
-});
+export const project = pgTable("project", {
+    id: serial("id").primaryKey(),
+    project_uuid: text("project_uuid").notNull(),
+    user_uuid: text("user_uuid").notNull(),
+    name: text("name"),
+    icon: text("icon"),
+    description: text("description"),
+    actual_date_of_creation: text("actual_date_of_creation"),
+    created_at: timestamp("created_at").notNull().defaultNow(),
+    updated_at: timestamp("updated_at").notNull().defaultNow(),
+})
 
 export const chatMessages = pgTable("chat_messages", {
   id: serial("id").primaryKey(),
-  chat_id: integer("chat_id")
+  project_uuid: text("project_uuid")
     .notNull()
-    .references(() => chat.id, { onDelete: "cascade" }),
+    .references(() => project.project_uuid, { onDelete: "cascade" }),
   role: text("role").notNull(),
   content: text("content").notNull(),
   created_at: timestamp("created_at").notNull().defaultNow(),
