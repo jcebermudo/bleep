9187d3e16ff9adf903efe08d3e612f984edd4208
James <jamestech718@gmail.com>
2025-06-01 00:30:26 +0800

    puzzle

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Link from "./components/link";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import Projects from "./components/projects";
+import Image from "next/image"
+import * as motion from "motion/react-client";
 
 export default async function Home() {
   const supabase = await createClient();
@@ -12,19 +14,48 @@ export default async function Home() {
   }
   return (
     <div className="w-full h-full flex flex-col items-center px-[10px] justify-center bg-black">
-      <div className="div-w-scrollbar bg-[#070707] w-full h-screen overflow-y-auto">
-        <div className="flex flex-col items-center">
-          <h1 className="text-[32px] font-medium">
-            Generate Chrome extension ideas
-          </h1>
-          <p className="text-[16px] font-medium m-[10px] text-white max-w-[468.47px] text-center">
-            Collect bad reviews from any Chrome extension and use AI to get
-            useful insights and new product ideas.
-          </p>
-          <Link userId={data.user.id} />
-        </div>
-        <div className="mt-[30px] flex flex-col items-center">
-          <Projects userId={data.user.id} />
+      <div className="rounded-t-[20px] outline-[1px] outline-[#2D2D2D] bg-[#070707] w-full h-screen overflow-y-auto">
+        <div className="flex flex-col items-center mt-[100px]">
+          <div className="relative">
+              <motion.div
+        className="select-none absolute translate-x-[120%] translate-y-[40%]"
+        animate={{
+          y: [-8, 8, -8],
+        }}
+        transition={{
+          duration: 3,
+          repeat: Infinity,
+          ease: "easeInOut"
+        }}
+      >
+        <Image
+          src="/images/puzzle.svg"
+          alt="puzzle"
+          width={216.38}
+          height={166.04}
+        />
+      </motion.div>
+            <Image
+              className="select-none"
+              src="/images/graph-website.svg"
+              alt="graph"
+              width={692.82}
+              height={400}
+            />
+          </div>
+          <div className="mt-[-120px] flex flex-col items-center z-[10]">
+            <h1 className="text-[32px] font-medium">
+              Generate Chrome extension ideas
+            </h1>
+            <p className="text-[16px] font-medium m-[10px] text-white max-w-[468.47px] text-center">
+              Collect bad reviews from any Chrome extension and use AI to get
+              useful insights and new product ideas.
+            </p>
+            <Link userId={data.user.id} />
+          </div>
+          <div className="mt-[30px] flex flex-col items-center">
+            <Projects userId={data.user.id} />
+          </div>
         </div>
       </div>
     </div>
