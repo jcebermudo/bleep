61b1f99ec0228467cc030482ac916636a3ed86e3
James <jamestech718@gmail.com>
2025-06-04 23:52:12 +0800

    use state import

--- app/testing/page.tsx
+++ app/testing/page.tsx
@@ -1,3 +1,10 @@
-export default function () {
-    
+import { useState } from "react"
+
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Testing</h1>
+        </div>
+    )
 }
