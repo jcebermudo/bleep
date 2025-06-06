e5f9394d1706856ae5c49a1ea8e418f8c5490dbf
James <jamestech718@gmail.com>
2025-06-07 00:18:28 +0800

    added user component and logo link to page layout

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,6 +3,9 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import MainUI from "../components/mainui";
+import Image from "next/image";
+import User from "../components/user";
+import Link from "next/link";
 
 export default async function Page({
   params,
@@ -16,5 +19,28 @@ export default async function Page({
     redirect("/login");
   }
 
-  return <MainUI slug={slug} userId={data.user.id} />;
+  return (
+    <div className="w-full min-h-screen flex flex-col bg-black px-[10px]">
+      <div className="w-full flex flex-row justify-between items-center mt-[10px]">
+        <Link href="/">
+          <Image
+            className="select-none"
+            src="/images/BLEEP-LOGO.svg"
+            alt="bleep-logo"
+            width={90}
+            height={26.2}
+          />
+        </Link>
+        <div className="flex flex-row items-center">
+          <User
+            userId={data.user.id}
+            avatar_url={data.user.user_metadata.avatar_url}
+            name={data.user.user_metadata.name}
+            email={data.user.user_metadata.email}
+          />
+        </div>
+      </div>
+      <MainUI slug={slug} userId={data.user.id} />
+    </div>
+  );
 }
